be047b2d601a964611cc26c7e53cbdb82615e4d9
David Ciaffoni <david@dkc.space>
2025-10-09 17:54:54 -0700

    Initial Commit

---
+++ blackHoleTests/blackHoleTests.swift
@@ -0,0 +1,17 @@
+//
+//  blackHoleTests.swift
+//  blackHoleTests
+//
+//  Created by David Ciaffoni on 10/9/25.
+//
+
+import Testing
+@testable import blackHole
+
+struct blackHoleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
